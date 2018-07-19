7184c66d3db169b3b9ef68880e32bc2ea6f9d2f4
maskleo <masque@aliyun.com>
2018-07-19 23:00:54 +0800

    :tada: init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # java-guava-explain
 explain for Guava
+
+## LICENSE
+
+### [CC-BY-SA-3.0](https://creativecommons.org/licenses/by-nc-sa/3.0/cn/)
+
+[![](LICENSE.png)](https://creativecommons.org/licenses/by-nc-sa/3.0/cn/)
---
+++ guava-collections/src/main/java/edu/maskleo/guavacollections/GuavaCollectionsApplication.java
@@ -0,0 +1,12 @@
+package edu.maskleo.guavacollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaCollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavaCollectionsApplication.class, args);
+    }
+}
